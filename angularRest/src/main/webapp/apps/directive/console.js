3dfd93761cb4ca9996a62f5b59bfffa2748348db
crmerlino <crmerlino@gmail.com>
2020-05-08 08:34:56 -0400

    ok

--- angularRest/src/main/webapp/apps/directive/console.js
+++ angularRest/src/main/webapp/apps/directive/console.js
@@ -3,8 +3,7 @@ app.directive("w3TestDirective", function() {
         template : "<h1>TESTING:</h1>" +
         		
         		"<textarea name='message' style='background-color:black; color:lightgreen; width:700px; height:200px;'>{{outLog}}</textarea>" +
-        		"<br>" 
-        		   		
+        		"<br>" 		   		
     };
 });
 
